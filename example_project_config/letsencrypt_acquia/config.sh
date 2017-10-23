343b04d4e27a6c30c789258bdb75cff61378bba8
Radim Klaska <radim@klaska.net>
2017-10-23 23:57:42 +0200

    Add proper readme and exmpale config.

---
+++ example_project_config/letsencrypt_acquia/config.sh
@@ -0,0 +1,3 @@
+# @See: https://github.com/lukas2511/dehydrated/blob/master/docs/examples/config
+# Only overrides here.
+CONTACT_EMAIL="contact+PROJECT@morpht.com"
---
+++ example_project_config/letsencrypt_acquia/slack.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# Get it here: https://my.slack.com/services/new/incoming-webhook/
+webhook_url='https://hooks.slack.com/services/XXXXXXXXX/XXXXXXXXX/XXXXXXXXXXXXXXXXXXXXXXXX'
+channel='CHANNEL-NAME'
